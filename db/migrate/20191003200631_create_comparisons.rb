ffb4fa445eacae8810680e9f978b0ed94de679f8
Owen Moss <owenkmoss@gmail.com>
2019-10-11 09:24:32 +0200

    setup triple join

--- db/migrate/20191003200631_create_comparisons.rb
+++ db/migrate/20191003200631_create_comparisons.rb
@@ -3,6 +3,7 @@ class CreateComparisons < ActiveRecord::Migration[5.2]
     create_table :comparisons do |t|
       t.integer :superior_movie_id, foreign_key: true
       t.integer :inferior_movie_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-Movie.destroy_all
 Comparison.destroy_all
+Movie.destroy_all
+User.destroy_all
+
+christopher = User.create(name: "Christopher Nolan")
+quentin = User.create(name: "Quentin Tarantino")
 
 dark_night = Movie.create(title: "The Dark Knight", year: 2008)
 pulp_fiction = Movie.create(title: "Pulp Fiction", year: 1994)
 citizen_kane = Movie.create(title: "Citizen Kane", year: 1941)
 
-Comparison.create(superior_movie_id: dark_night.id, inferior_movie_id: citizen_kane.id)
-Comparison.create(superior_movie_id: citizen_kane.id, inferior_movie_id: pulp_fiction.id)
+Comparison.create(user_id: christopher.id, superior_movie_id: dark_night.id, inferior_movie_id: citizen_kane.id)
+Comparison.create(user_id: christopher.id, superior_movie_id: citizen_kane.id, inferior_movie_id: pulp_fiction.id)
+Comparison.create(user_id: quentin.id, superior_movie_id: citizen_kane.id, inferior_movie_id: dark_night.id)
+Comparison.create(user_id: quentin.id, superior_movie_id: pulp_fiction.id, inferior_movie_id: citizen_kane.id)
